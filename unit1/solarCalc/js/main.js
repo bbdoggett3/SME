5684a25ea371b06f71dd74133d51ee40c669e01e
bbdoggett3 <bbdoggett3@gmail.com>
2021-06-29 11:28:00 -0600

    create functional calculator

--- unit1/solarCalc/js/main.js
+++ unit1/solarCalc/js/main.js
@@ -1,2 +1,29 @@
 //Javascript Document
-alert("this document is working")
+function floor(diameter) {
+    let radius = diameter/2;
+    let floorArea = Math.PI * (radius * radius);
+
+    return floorArea;
+} //end of function
+
+function walls(diameter, height) {
+    let radius = diameter/2;
+    let circumference = 2 * Math.PI * radius;
+    let wallArea = circumference * height;
+
+    return wallArea;
+} //end of function
+
+function materialsNeeded() {
+    let d = document.getElementById('across').value;
+    let h = document.getElementById('height').value;
+
+    let carpetNeeded = Math.ceil(floor(d));
+    let paintNeeded = Math.ceil(walls(d, h));
+
+    console.log(carpetNeeded);
+    console.log(paintNeeded);
+
+
+} //end of function
+
