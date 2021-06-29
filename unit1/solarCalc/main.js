40389667c5f6405272b57eb73cd99d5a25674966
bbdoggett3 <bbdoggett3@gmail.com>
2021-06-29 16:03:44 -0600

    create new functions for the  project with calculators

--- unit1/solarCalc/main.js
+++ unit1/solarCalc/main.js
@@ -1,1 +1,5 @@
-//Javascript for Solar Calculator
+//Javascript for Solar Calculator"
+
+var annualUseKw = 0, dailyUseKw = 0, i = 0, x = 0;
+var months = document.getElementById('mpc').getElementsByTagName('input');
+console.log(months);
